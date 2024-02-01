8239e861d79d7f3c02f4a37f575c1fe48cc5f4a3
Devin <71668732+Itx-Dev@users.noreply.github.com>
2024-02-01 13:08:25 -0500

    Progress
    Weird bugs when doing "./a.out b a a a a b b b a a c b c"
    But "./a.out is a very very interesting program" works

--- Lab1/main.c
+++ Lab1/main.c
@@ -3,56 +3,34 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    // Length of arguments
-    int length = argc;
-    char usedWords[32][15];
-    char tempArray[32][15];
-
-    // Initialize Array to NULL Terminator
-    for (int row = 0; row < 32; row++) {
-        for (int col = 0; col < 15; col++) {
-            usedWords[row][col] = '\0';
-            tempArray[row][col] = '\0';
-        }
-    }
-
-    for (int currentWordIndex = 1; currentWordIndex < length; currentWordIndex++) {
-        // Reset occurances every target word change
-        int occurances = 0;
-
-        char* currentWord = argv[currentWordIndex];
- 
-        // Word to find
-        for (int targetWordIndex = 1; targetWordIndex < length; targetWordIndex++) {
-            char* targetWord = argv[targetWordIndex];
-
-            // If word matches argument
-            if (strcmp(currentWord, targetWord) == 0) {
-                occurances++;
-            }
-         }
-        // Copy used word to new array
-        strcpy(usedWords[currentWordIndex - 1], argv[currentWordIndex]);
-
-    }
+    char tempArray[32][32];
+    int occurancesCount[32];
 
     // Create array of strings without duplicates
     int count = 0;
         
-    for (int i = 0; i < length - 1; i++) {
-        int j;
-        for (j = 0; j < count; j++) {
-            if (strcmp(usedWords[i], tempArray[j]) == 0) {
+    for (int currentWord = 0; currentWord < argc; currentWord++) {
+        int occurances = 1;
+        int occuranceIndex = 0;
+
+        int targetWord;
+        for (targetWord = 0; targetWord < count; targetWord++) {
+
+            if (strcmp(argv[currentWord], argv[targetWord]) == 0) {
+                ++occurances;
                 break;
             }
         }
-        if (j == count) {
-            strcpy(tempArray[count], usedWords[i]);
-            count++;
+        if (targetWord == count) {
+            strcpy(tempArray[count], argv[currentWord]);
+            ++count;
         }
+
+        occurancesCount[count - 1] = occurances;
+        printf("%d\n", count);
     }
 
     for (int k = 0 ; k < count; k++) {
-        printf("%s: %d\n", tempArray[k], count);
+        printf("%s: %d\n", tempArray[k], occurancesCount[k]);
     }
 }
