7be249183053b0071c9eaaf80135da3da32f4df0
Devin <dr2676@ship.edu>
2024-02-21 20:19:12 -0500

    Checkmark with working Batch and Interactive Mode

--- Lab2/main.c
+++ Lab2/main.c
@@ -1,10 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 #include "processShell.h"
 #include "error.h"
-#include <stdio.h>
 
 
 int main(int argc, char* args[]) {
     FILE *fp;
+    int rc = 0;
 
     if (argc > 1) {
         fp = fopen(args[1], "r");
@@ -16,4 +21,5 @@ int main(int argc, char* args[]) {
 
     processShell(fp);
 
+    return 0;
 }
