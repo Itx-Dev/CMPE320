fccc155dd163423efee1a4317875f04690bdaa33
Devin <dr2676@ship.edu>
2024-02-21 21:48:15 -0500

    Pass test 2

--- Lab2/processShell.c
+++ Lab2/processShell.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 #include "error.h"
 
@@ -76,139 +77,31 @@ int processShell(FILE* fp) {
         // Parse Command into string array
         stringArray = parseStringIntoArray(currentLine, stringArray);
 
+        // Define command
+        char* command = stringArray[0];
+
         // If User decides to Exit
-        if (strcmp(stringArray[0], "exit") == 0){
+        if (strcmp(command, "exit") == 0){
             exit(0);
         }
 
         // If User decides to CD
-        else if (strcmp(stringArray[0], "cd") == 0) {
+        else if (strcmp(command, "cd") == 0) {
+            char* command = stringArray[0];
+            char* directory[] = {command, stringArray[1], NULL};
             // Check for cd error
-            if (stringArray[1] == NULL) {
+            if (execv(command, directory) == -1) {
                 throwError();
-                return -1;
-            } else {
-                printf("%s", stringArray[1]);
-                chdir(stringArray[1]);
             }
         }
 
         // If User decides to PATH
-        else if (strcmp(stringArray[0], "path") == 0) {
+        else if (strcmp(command, "path") == 0) {
             exit(0);
         } 
 
-
         // Print results
-        index++;
-        //printf("Line %d: %s\n", index, originalString);
-    }
-}
-
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-
-#include "error.h"
-
-/**
- * @brief Remove new line character from string.
- * @param string 
- */
-void removeNewLine(char *string) {
-    if (string[strlen(string) - 1] == '\n') {
-        string[strlen(string) - 1] = '\0';
-    }
-}
-
-/**
- * @brief Break given command down into an array of strings and return the array of strings
- * 
- * @param givenLine 
- * @param storageArray 
- * @return char** Array of Strings with current line
- */
-char** parseStringIntoArray(char *givenLine, char** storageArray) {
-    char* splitString = NULL;
-    int argumentIndex = 0;
-
-    // Split String into tokens by spaces
-    splitString = strtok(givenLine, " ");
-    removeNewLine(splitString);
-
-    // Parse string given by user or batch file and split into string array
-    while (splitString != NULL) {
-
-        // Store split string into a string array
-        storageArray[argumentIndex] = splitString;
-
-        // Test for NULL pointer
-        splitString = strtok(NULL, " ");
-        argumentIndex++;
-    }
-
-    return storageArray;
-}
-
-int processShell(FILE* fp) {
-    char* currentLine = NULL;
-    size_t lineLength = 0;
-    size_t read;
-    int index = 0;
-
-    while (1) {
-        // Print prompt only if in batch mode
-        if (fp == stdin) {
-            printf("wish> ");
-        }
-
-        // Read in current Line
-        read = getline(&currentLine, &lineLength, fp);
-
-        // If EOF is reached exit
-        if (read == -1) {
-            exit(0);
-        }
-        
-        // Create Space to store original string
-        char* originalString = (char*)malloc(lineLength);
-        strcpy(originalString, currentLine);
-
-        removeNewLine(originalString);
-
-        // Create Space to store command
-        char** stringArray = malloc(32 * sizeof(char*));
-
-        // Parse Command into string array
-        stringArray = parseStringIntoArray(currentLine, stringArray);
-
-        // If User decides to Exit
-        if (strcmp(stringArray[0], "exit") == 0){
-            exit(0);
-        }
-
-        // If User decides to CD
-        else if (strcmp(stringArray[0], "cd") == 0) {
-            // Check for cd error
-            if (stringArray[1] == NULL) {
-                throwError();
-                return -1;
-            } else {
-                printf("%s", stringArray[1]);
-                chdir(stringArray[1]);
-            }
-        }
-
-        // If User decides to PATH
-        else if (strcmp(stringArray[0], "path") == 0) {
-            exit(0);
-        } 
-
-
-        // Print results
-        index++;
+        //index++;
         //printf("Line %d: %s\n", index, originalString);
     }
 }
