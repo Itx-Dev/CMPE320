523c68c15b368d3b73f062e20dc8315bafee6880
Devin <dr2676@ship.edu>
2024-02-25 23:12:54 -0500

    Fixed hardcoded ls error

--- Lab2/otherCommands.c
+++ Lab2/otherCommands.c
@@ -9,54 +9,45 @@
 
 void otherCommands(char **stringArray, char *command, char **mainDirectory)
 {
+    char* currentDirectory = stringArray[1];
     // If no path is given for ls use currentDirectory
     if (stringArray[1] == NULL && strcmp(command, "ls") == 0)
     {
-        // Get current working directory
-        char currentDirectory[120];
         getcwd(currentDirectory, sizeof(currentDirectory));
         stringArray[1] = currentDirectory;
     }
 
-    // Check if directory is accessible for ls
-    if (access(stringArray[1], F_OK) == -1 && strcmp(command, "ls") == 0)
+    // Fork command process
+    pid_t pid = fork();
+
+    // Child process
+    if (pid == 0)
     {
-        fprintf(stderr, "%s: cannot access '%s': %s\n", command, stringArray[1], strerror(errno));
+        // Combine bin path to executable path
+        int pathIndex = 0;
+        int misses = 0;
+        while (mainDirectory[pathIndex] != NULL)
+        {
+            char fullPath[100];
+            snprintf(fullPath, sizeof(fullPath), "%s/%s", mainDirectory[pathIndex], command);
+            // Define arguments
+            char *args[] = {command, currentDirectory, NULL};
+            // execv should not return if it does, error
+            if (execv(fullPath, args) == -1)
+            {
+                misses++;
+            }
+            pathIndex++;
+        }
+        // If all directories fail throw error
+        if (misses == pathIndex)
+        {
+            throwError();
+        }
+        exit(0);
     }
     else
     {
-        // Fork command process
-        pid_t pid = fork();
-
-        // Child process
-        if (pid == 0)
-        {
-            // Combine bin path to executable path
-            int pathIndex = 0;
-            int misses = 0;
-            while (mainDirectory[pathIndex] != NULL)
-            {
-                char fullPath[100];
-                snprintf(fullPath, sizeof(fullPath), "%s/%s", mainDirectory[pathIndex], command);
-                // Define arguments
-                char *args[] = {fullPath, NULL};
-                // execv should not return if it does, error
-                if (execv(fullPath, args) == -1)
-                {
-                    misses++;
-                }
-                pathIndex++;
-            }
-            // If all directories fail throw error
-            if (misses == pathIndex)
-            {
-                throwError();
-            }
-            exit(0);
-        }
-        else
-        {
-            waitpid(pid, NULL, 0);
-        }
+        waitpid(pid, NULL, 0);
     }
 }
