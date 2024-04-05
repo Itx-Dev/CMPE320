fc229037eefcbaa4dcd7c2baab64bca03728ef02
Devin <dr2676@ship.edu>
2024-04-05 14:14:51 -0400

    Pass up to test4 implemented fwrite() rather than printf()

--- Project2/wzip.c
+++ Project2/wzip.c
@@ -1,28 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <pthread.h>
 
 #define MAXLENGTH 50
  
 char count[5] = {'0'};
-int numberOfCharacters = 0;
-
 char* encode(char* givenString)
 {
     int characterCount;
     // Find the occurances of a character
     for (int i = 0; i < strlen(givenString); i++) {
-        count[4] = givenString[i];
         // Count occurances of character
         characterCount = 1;
         while (i + 1 < strlen(givenString) && givenString[i] == givenString[i + 1]) {
+            count[4] = givenString[i];
             characterCount++;
             i++;
         }
+        // Put each character in count array
         sprintf(count, "%c", characterCount);
     }
-    numberOfCharacters++;
 
     return count;
 }
@@ -35,28 +32,41 @@ int main(int argc, char* args[])
     }
 
     size_t lineLength = 0;
-    size_t successfulGetLine;
+    size_t successfulLine;
+    FILE *fp;
 
     char* currentLine = NULL;
     char* concatenatedLine = malloc(32 * sizeof(char));
+    // Set first file name
+    char* filename = args[1];
 
-    for (int i = 1; i < argc; i++) {
-        char* filename = (char*)args[i];
-
-        FILE* fp = fopen(filename, "r");
-        
-        successfulGetLine = getline(&currentLine, &lineLength, fp);    // Read in current Line
-        strcat(concatenatedLine, currentLine);
-
-        if (successfulGetLine == -1) {
-            return 1;
+    // Encode concatenated Line
+    if (argc > 2) {
+        for (int i = 1; i < argc; i++) {
+            // Change file name for multiple files
+            filename = args[i];
+            fp = fopen(filename, "r");
+            // Get lines
+            successfulLine = getline(&currentLine, &lineLength, fp);
+            if (successfulLine != -1) {
+                strcat(concatenatedLine, currentLine);
+            }
         }
+
+        char* response = encode(concatenatedLine);
+        fwrite(response, 1, 5, stdout);
+        fclose(fp);
+    } 
+    // Encode if there is only one file
+    else 
+    {
+        fp = fopen(filename, "r");
+        while (getline(&currentLine, &lineLength, fp) != -1) {
+            char *response = encode(currentLine);
+            fwrite(response, 1, 5, stdout);
+        }
+        fclose(fp);
     }
 
-    char* response = encode(concatenatedLine);
-
-    for (int i = 0; i < 5; i++) {
-        printf("%c", response[i]);
-    }
     return 0;    
 }
