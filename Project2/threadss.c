2a5b76eb95d3f2812c473a58e4d3fa10ef5008d1
Devin <devinreichenbach@yahoo.com>
2024-04-13 09:27:27 -0400

    Implement structs for threads
    Structs store arguments and return values for each thread.

--- Project2/threadss.c
+++ Project2/threadss.c
@@ -9,17 +9,27 @@
 // Define a mutex for synchronization
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
+
 typedef struct {
-    char* rawString;
-    int start;  // Start index of the section
-    int length; // Length of the section
-} ThreadArgs;
+    struct arg_val_t {
+        char* rawString;
+        int start;
+        int length;
+    } arg_val; 
+    struct ret_val_t {
+        char firstChar;
+        char lastChar;
+        int firstCharCount;
+        int lastCharCount;
+    } ret_val;
+} arg_t;
 
 void* threadFunction(void* arg) {
-    ThreadArgs* threadArgs = (ThreadArgs*)arg;
-    char* rawString = threadArgs->rawString;
-    int start = threadArgs->start;
-    int length = threadArgs->length;
+    arg_t* argStructs = (arg_t*)arg; // Take in parameter and result structures
+
+    char* rawString = argStructs->arg_val.rawString;
+    int start = argStructs->arg_val.start;
+    int length = argStructs->arg_val.length;
 
     char currentChar, nextChar;
     int count = 1;
@@ -33,20 +43,13 @@ void* threadFunction(void* arg) {
             count++;
         } else {
             pthread_mutex_lock(&mutex); // Lock the mutex before writing
-            putchar(count);
-            putchar(currentChar);
             pthread_mutex_unlock(&mutex); // Unlock the mutex after writing
             count = 1;  // Reset count for new character
         }
-
         currentChar = nextChar;
     }
-
-    // Write the last character count and character
-    pthread_mutex_lock(&mutex); // Lock the mutex before writing
-    printf("count: %d character: %c\n",count, currentChar);
-    pthread_mutex_unlock(&mutex); // Unlock the mutex after writing
-
+    argStructs->ret_val.lastChar = currentChar;
+    argStructs->ret_val.lastCharCount = count;
     pthread_exit(NULL);
 }
 
@@ -54,33 +57,35 @@ int main(int argc, char* args[]) {
     FILE* inputFile;
 
     if (argc > 2) {
-        for (int file = 0; file < argc; file++) {
-            printf("File %d:\n", file);
-            inputFile = fopen(args[2], "rb");
-            int fd = fileno(inputFile);
-            struct stat st;
-            fstat(fd, &st);
-            int fsize = st.st_size;
-            char* string = mmap(0, fsize, PROT_READ, MAP_PRIVATE, fd, 0);
-            int stringLength = strlen(string);
-            int numOfThreads = 3;
-            int sectionLength = stringLength / numOfThreads;
+        for (int file = 1; file < argc; file++) {
+                printf("File %d:\n", file);
+                inputFile = fopen(args[2], "rb");
+                int fd = fileno(inputFile);
+                struct stat st;
+                fstat(fd, &st);
+                int fsize = st.st_size;
+                char* string = mmap(0, fsize, PROT_READ, MAP_PRIVATE, fd, 0);
+                int stringLength = strlen(string);
+                int numOfThreads = 3;
+                int sectionLength = stringLength / numOfThreads;
 
-            pthread_t threads[numOfThreads];
-            ThreadArgs threadArgs[numOfThreads];
+                pthread_t threads[numOfThreads];
+                arg_t threadParameter[numOfThreads];
+                
+                // Create threads, each responsible for processing a section of the string
+                for (int threadNum = 0; threadNum < numOfThreads; threadNum++) {
+                    threadParameter[threadNum].arg_val.rawString = string;
+                    threadParameter[threadNum].arg_val.start = threadNum * sectionLength;
+                    threadParameter[threadNum].arg_val.length = (threadNum == numOfThreads - 1) ? stringLength - threadParameter[threadNum].arg_val.start : sectionLength;
+                    threadParameter[threadNum].ret_val.lastCharCount = 0;
+                    pthread_create(&threads[threadNum], NULL, threadFunction, (void*)&threadParameter[threadNum]);
+                }
 
-            // Create threads, each responsible for processing a section of the string
-            for (int threadNum = 0; threadNum < numOfThreads; threadNum++) {
-                threadArgs[threadNum].rawString = string;
-                threadArgs[threadNum].start = threadNum * sectionLength;
-                threadArgs[threadNum].length = (threadNum == numOfThreads - 1) ? stringLength - threadArgs[threadNum].start : sectionLength;
-                pthread_create(&threads[threadNum], NULL, threadFunction, (void*)&threadArgs[threadNum]);
-            }
-
-            // Wait for all threads to complete
-            for (int waitingThread = 0; waitingThread < numOfThreads; waitingThread++) {
-                pthread_join(threads[waitingThread], NULL);
-            }
+                // Wait for all threads to complete
+                for (int waitingThread = 0; waitingThread < numOfThreads; waitingThread++) {
+                    pthread_join(threads[waitingThread], NULL);
+                    printf("Last Character: %c has %d occurances\n", threadParameter[waitingThread].ret_val.lastChar, threadParameter[waitingThread].ret_val.lastCharCount);
+                }
         }
     }
 
