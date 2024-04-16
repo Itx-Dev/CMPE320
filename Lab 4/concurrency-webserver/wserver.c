dbb107887aab69b000991633fe4b126691cb9f90
Devin <dr2676@ship.edu>
2024-04-16 07:30:04 -0400

    Add parsing options (-t,-b,-s)

--- Lab 4/concurrency-webserver/wserver.c
+++ Lab 4/concurrency-webserver/wserver.c
@@ -2,6 +2,7 @@
 #include <pthread.h>
 #include "request.h"
 #include "io_helper.h"
+#include <getopt.h>
 
 void *mythread(void* conn_fd) {
 	int connection = (int)conn_fd;
@@ -19,8 +20,12 @@ int main(int argc, char *argv[]) {
     int c;
     char *root_dir = default_root;
     int port = 10000;
+	int threadNum = 1;
+	int buffer = 1;
+	char* schedalg = malloc(5 * sizeof(char)); 
+	strcpy(schedalg, "FIFO");
     
-    while ((c = getopt(argc, argv, "d:p:")) != -1)
+    while ((c = getopt(argc, argv, "d:p:t:b:s")) != -1)
 	switch (c) {
 	case 'd':
 	    root_dir = optarg;
@@ -28,10 +33,32 @@ int main(int argc, char *argv[]) {
 	case 'p':
 	    port = atoi(optarg);
 	    break;
+	case 't':
+		threadNum = atoi(optarg);
+		if (threadNum <= 0) {
+			exit(-1);
+		}
+		break;
+	case 'b':
+		buffer = atoi(optarg);
+		if (buffer <= 0) {
+			exit(-1);
+		}
+		break;
+	case 's':
+		if (strcmp(argv[optind], "FIFO") == 0 || strcmp(argv[optind], "SFF") == 0) {
+			strncpy(schedalg, argv[optind], 4);
+		} else {
+			exit(-1);
+		}
+		break;
+	
 	default:
-	    fprintf(stderr, "usage: wserver [-d basedir] [-p port]\n");
+	    fprintf(stderr, "usage: wserver [-d basedir] [-p port] [-t threads] [-b buffer] [-s scheduling algorithm\n");
 	    exit(1);
 	}
+ 	printf("Thread Number: %d\nBuffer Number: %d\nScheduling Algorithm: %s\n", threadNum, buffer, schedalg);   
+	free(schedalg);
 
     // run out of this directory
     chdir_or_die(root_dir);
@@ -39,12 +66,12 @@ int main(int argc, char *argv[]) {
     // now, get to work
     int listen_fd = open_listen_fd_or_die(port);
     while (1) {
-	struct sockaddr_in client_addr;
-	int client_len = sizeof(client_addr);
-	int conn_fd = accept_or_die(listen_fd, (sockaddr_t *) &client_addr, (socklen_t *) &client_len);
-	pthread_t thread;
-	pthread_create(&thread, NULL,(void*) mythread, (void*)conn_fd);
-    }
+		struct sockaddr_in client_addr;
+		int client_len = sizeof(client_addr);
+		int conn_fd = accept_or_die(listen_fd, (sockaddr_t *) &client_addr, (socklen_t *) &client_len);
+		pthread_t thread;
+		pthread_create(&thread, NULL,(void*) mythread, (void*)conn_fd);	
+	}
     return 0;
 }
 
