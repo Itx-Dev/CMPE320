f81b3c113f6847dce18f1c1002011a8835d507b8
Devin <dr2676@ship.edu>
2024-04-17 06:16:27 -0400

    Lab 5

--- Lab4-5/Concurrent Queue/main.c
+++ Lab4-5/Concurrent Queue/main.c
@@ -4,54 +4,41 @@
 
 #include "queue.h"
 
-void* threadEnqueue1(void *argt) {
+int val1 = 1, val2 = 2, val3 = 3;
+
+void* threadEnqueue(void *argt) {
     queue_t *queue = (queue_t *)argt;
-    int val1 = 1, val2 = 2, val3 = 3;
-    Queue_Enqueue(queue, val1);
-    sleep(1);
-    Queue_Enqueue(queue, val2);
-    sleep(2);
-    Queue_Enqueue(queue, val3);
+
+    // Enqueue
+    for (int i = 1; i < 4; ++i) {
+        Queue_Enqueue(queue, i); // Enqueue item number i
+    }
+
     return NULL;
 }
 
-void* threadEnqueue2(void *argt) {
+void* threadDequeue(void *argt) {
     queue_t *queue = (queue_t *)argt;
-    int val1 = 4, val2 = 5, val3 = 6;
-    Queue_Enqueue(queue, val1);
-    sleep(1);
-    Queue_Enqueue(queue, val2);
-    sleep(1);
-    Queue_Enqueue(queue, val3);
-    return NULL;
+
+    Empty_Queue(queue);
 }
 
-void* threadEnqueue3(void *argt) {
-    queue_t *queue = (queue_t *)argt;
-    int val1 = 7, val2 = 8, val3 = 9;
-    Queue_Enqueue(queue, val1);
-    sleep(4);
-    Queue_Enqueue(queue, val2);
-    sleep(2);
-    Queue_Enqueue(queue, val3);
-    return NULL;
-}
 
 
 int main(int argc, char* argv[]) {
     int numThreads = 3;
-    pthread_t thread1, thread2, thread3;
+    pthread_t threads[numThreads];
     queue_t *queue = malloc(sizeof(queue_t));
 
     Queue_Init(queue);
 
-    pthread_create(&thread1, NULL, threadEnqueue1, (void*)queue);
-    pthread_create(&thread2, NULL, threadEnqueue2, (void*)queue);
-    pthread_create(&thread3, NULL, threadEnqueue3, (void*)queue);
+    for (int i = 0; i < numThreads; ++i) {
+        pthread_create(&threads[i], NULL, threadEnqueue, (void*)queue);
+    }
 
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
-    pthread_join(thread3, NULL);
+    for (int i = 0; i < numThreads; ++i) {
+        pthread_join(threads[i], NULL);
+    }
 
     Empty_Queue(queue);
 
