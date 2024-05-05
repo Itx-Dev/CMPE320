b467a53bd8f4aa651741e338981ccd09579c4212
Devin Reichenbach <dr2676@engr.ship.edu>
2024-05-05 16:52:29 -0400

    Properly format main

--- Lab4-5/concurrency-webserver/wserver.c
+++ Lab4-5/concurrency-webserver/wserver.c
@@ -21,42 +21,42 @@ int main(int argc, char *argv[]) {
     int c;
     char *root_dir = default_root;
     int port = 10000;
-	int threadNum = 1;
-	int buffer = 1;
-	char* schedalg = "FIFO"; 
-    
-  while ((c = getopt(argc, argv, "d:p:t:b:s")) != -1)
-	switch (c) {
-		case 'd':
-			root_dir = optarg;
-			break;
-		case 'p':
-			port = atoi(optarg);
-			break;
-		case 't':
-			threadNum = atoi(optarg);
-			if (threadNum <= 0) {
-				exit(-1);
-			}
-			break;
-		case 'b':
-			buffer = atoi(optarg);
-			if (buffer <= 0) {
-				exit(-1);
-			}
-			break;
-		case 's':
-			if (strcmp("FIFO", optarg) == 0) { schedalg = "FIFO"; }
-			else if (strcmp("SFF", optarg) == 0) { schedalg = "SFF"; }
-			else { exit(-1); }
-			break;
+		int threadNum = 1;
+		int buffer = 1;
+		char* schedalg = "FIFO"; 
 			
-		default:
-			fprintf(stderr, "usage: wserver [-d basedir] [-p port] [-t threads] [-b buffer] [-s scheduling algorithm\n");
-			exit(1);
-	}
-			
- 	printf("Thread Number: %d\nBuffer Number: %d\nScheduling Algorithm: %s\n", threadNum, buffer, schedalg);   
+		while ((c = getopt(argc, argv, "d:p:t:b:s")) != -1)
+		switch (c) {
+			case 'd':
+				root_dir = optarg;
+				break;
+			case 'p':
+				port = atoi(optarg);
+				break;
+			case 't':
+				threadNum = atoi(optarg);
+				if (threadNum <= 0) {
+					exit(-1);
+				}
+				break;
+			case 'b':
+				buffer = atoi(optarg);
+				if (buffer <= 0) {
+					exit(-1);
+				}
+				break;
+			case 's':
+				if (strcmp("FIFO", optarg) == 0) { schedalg = "FIFO"; }
+				else if (strcmp("SFF", optarg) == 0) { schedalg = "SFF"; }
+				else { exit(-1); }
+				break;
+				
+			default:
+				fprintf(stderr, "usage: wserver [-d basedir] [-p port] [-t threads] [-b buffer] [-s scheduling algorithm\n");
+				exit(1);
+		}
+				
+		printf("Thread Number: %d\nBuffer Number: %d\nScheduling Algorithm: %s\n", threadNum, buffer, schedalg);   
 
     // run out of this directory
     chdir_or_die(root_dir);
@@ -64,7 +64,7 @@ int main(int argc, char *argv[]) {
     // now, get to work
     int listen_fd = open_listen_fd_or_die(port);
 
-	thread_pool_init(threadNum); // Initialize thread pool
+		thread_pool_init(threadNum); // Initialize thread pool
 
     while (1) {
 		struct sockaddr_in client_addr;
